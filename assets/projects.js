59dd63309de306c9816ce155f55f9d3d73902384
seanpohyx <seanpohyx@gmail.com>
2021-05-17 01:07:27 +0800

    version 1

---
+++ assets/projects.js
@@ -0,0 +1,48 @@
+var sw_projects = [
+  {
+  	"title": "HopeUni-Testimony",
+  	"desc": "This project is constructed for my church community. I created a platform that allowed for people to share their testimony and encouragements to one other during the exam period. Currently taken down to improve it further.", 
+  	"href": "https://github.com/seanpohyx/hopeUni-Testimony", 
+  	"img": "./assets/images/nyc-testimony.jpg"
+  },
+  {
+  	"title": "HopeUni-Testimony-backend",
+  	"desc": "First Attempt to build a RESTful API using Flask and Heroku", 
+  	"href": "https://github.com/seanpohyx/hopeUni-testimony-backend", 
+  	"img": "./assets/images/flask.jpg"
+  },
+  {
+  	"title": "Kinderfind",
+  	"desc": "This project is created for software engineering module in NTU. we created an android application that allows parents to locate the nearest kindergarten that is best for their children. They are able to rate and give feedbacks for other parents to view.", 
+  	"href": "https://github.com/seanpohyx/kinderfind/", 
+  	"img": "./assets/images/kinderfind.jpg"
+  },
+  {
+  	"title": "Facial-recognition-Attendance-Taking-System",
+  	"desc": "This project is created for Advanced software engineering module in NTU. We created a platform that uses facial recognition to for attendance taking.", 
+  	"href": "https://github.com/seanpohyx/Facial-recognition-Attendance-Taking-System", 
+  	"img": "./assets/images/flask.jpg"
+  },
+  {
+  	"title": "Unity Escape Room",
+  	"desc": "This project is created for my Software Systems Analysis & Design module. My first attempt in creating a game using Unity. My group was inspired by the concept of escape room and we tried building a game out of it.", 
+  	"href": "https://github.com/NTU-SCSE-CZ3003-SSAD-Player-1", 
+  	"img": "./assets/images/unity.jpg"
+  },
+  {
+  	"title": "Love Nanyang Campus",
+  	"desc": "This project is constructed for my church community. We wanted to make a commitment to bless the students within NTU starting with our friends. It started with us filling up google forms and the friends that we wished to bless within this semester. But, this needed a form of visual representation to motivate us. Therefore, inspiring to start this simple website.", 
+  	"href": "https://github.com/seanpohyx/love-nyc", 
+  	"img": "./assets/images/love-nyc.jpg"
+  }
+
+]
+
+var ml_projects = [
+  {
+  	"title": "Opinion-based intelligent Recommender System",
+  	"desc": "This is my Final Year Project for academic year 2020/2021 in Nanyang Technological University. The task is to develop a recommender system that learn user preferences through texts.", 
+  	"href": "https://github.com/seanpohyx/Opinion-based-intelligent-Recommender-System", 
+  	"img": "./assets/images/opinion.jpg"
+  }
+]
---
+++ static/script.js
@@ -0,0 +1,149 @@
+$( document ).ready(function() {
+	var sw_btn = $("#sw_proj_btn");
+	var ml_btn = $("#ml_proj_btn");
+	var isLoadMore = false;
+
+	generateProjects();
+
+	sw_btn.click(()=>{
+		if(!sw_btn.hasClass("active")){
+			sw_btn.addClass("active");
+			ml_btn.removeClass("active");
+
+			$('#Sw_project_content').removeClass("d-none");
+			$('#Ml_project_content').addClass("d-none");
+
+			isLoadMore = false;
+			adjustProjectContainer(sw_projects, false);
+		}
+	});
+
+	ml_btn.click(()=>{
+		if(!ml_btn.hasClass("active")){
+			ml_btn.addClass("active");
+			sw_btn.removeClass("active");
+
+			$('#Ml_project_content').removeClass("d-none");
+			$('#Sw_project_content').addClass("d-none");
+
+			isLoadMore = false;
+			adjustProjectContainer(ml_projects, false);
+		}
+	});
+
+	$("#loadMoreBtn").click(()=>{
+		if(sw_btn.hasClass("active")){
+			isLoadMore ^= true;
+			let sentence = (isLoadMore)?"Load Less": "Load More";
+			adjustProjectContainer(sw_projects, isLoadMore);
+
+			$("#loadMoreBtn").html(sentence);
+		}
+		else{
+			isLoadMore ^= true;
+			let sentence = (isLoadMore)?"Load Less": "Load More";
+			adjustProjectContainer(ml_projects, isLoadMore);
+
+			$("#loadMoreBtn").html(sentence);
+		}
+	});
+
+	$('.navbar-nav>li>a').on('click', function(){
+	    $('.navbar-collapse').collapse('hide');
+	});
+
+	$(window).resize(function() {
+	    clearTimeout(window.resizedFinished);
+	    window.resizedFinished = setTimeout(function(){
+	    	if(sw_btn.hasClass("active")){
+				adjustProjectContainer(sw_projects, isLoadMore);
+			}
+			else{
+				adjustProjectContainer(ml_projects, isLoadMore);
+			}
+	    }, 250);
+	});
+
+
+});
+
+$(document).bind('afterready', function() {
+    // call your code here that you want to run after all $(document).ready() calls have run
+	adjustProjectContainer(sw_projects, false);
+});
+
+function spinner_toggle(isActive){
+
+	if(isActive)
+		$("#spinner").removeClass("d-none");
+	else
+		$("#spinner").addClass("d-none");
+
+}
+
+
+function generateProjects(){
+
+	var content = "";
+	for (let i=sw_projects.length-1; i>=0; i--){
+
+		content += '<div class="col-lg-4 col-md-6 d-flex align-items-stretch">';
+        content += '<div class="card mt-3 position-relative padding-btm-61">';
+        content += '<img class="card-img-top p-3" src="'+sw_projects[i].img+'" alt="Card image cap">';
+        content += '<div class="card-body">';
+        content += '<h5 class="card-title font-bebas-regular colour-orange fs-20">'+sw_projects[i].title+'</h5>';
+        content += '<p class="card-text font-hel font-15">'+sw_projects[i].desc+'</p>';
+        content += '<div class="card-footer position-absolute bottom-0 start-0"><a href="'+sw_projects[i].href+'" class="btn btn-primary font-hel fs-15"> <i class="bi bi-github"></i> Github</a>';
+        content += '</div></div></div></div>';
+	}
+
+	$('#Sw_project_content').html(content);
+
+	content = "";
+	for (let i=ml_projects.length-1; i>=0; i--){
+
+		content += '<div class="col-lg-4 col-md-6 d-flex align-items-stretch">';
+        content += '<div class="card mt-3 position-relative padding-btm-61">';
+        content += '<img class="card-img-top p-3" src="'+ml_projects[i].img+'" alt="Card image cap">';
+        content += '<div class="card-body">';
+        content += '<h5 class="card-title font-bebas-regular colour-orange fs-20">'+ml_projects[i].title+'</h5>';
+        content += '<p class="card-text font-hel font-15">'+ml_projects[i].desc+'</p>';
+        content += '<div class="card-footer position-absolute bottom-0 start-0"><a href="'+ml_projects[i].href+'" class="btn btn-primary font-hel fs-15"> <i class="bi bi-github"></i> Github</a>';
+        content += '</div></div></div></div>';
+	}
+
+	$('#Ml_project_content').html(content);
+
+	spinner_toggle(false);
+
+	setTimeout(function() {
+        $(document).trigger('afterready');
+    }, 100);
+
+}
+
+function adjustProjectContainer(project, isShow){
+	var container_width = $('#Project_container').width();
+	var content_div = (project == sw_projects)?$('#Sw_project_content .card'):$('#Ml_project_content .card');
+	var card_width = (project == sw_projects)?content_div.width():content_div.width();
+
+	var num_of_card_per_row = Math.floor(container_width/card_width);
+	let rows = (isShow)?Math.ceil(project.length/num_of_card_per_row):3;
+	let i=0;
+	let count=0;
+	var total_height = 0;
+
+	do{
+		total_height += content_div[i].offsetHeight;
+		i+=num_of_card_per_row;
+		count++;
+
+	}while(count<rows && i<project.length);
+
+	// $('#Project_container').css("height", total_height + rows * 16 + "px");
+
+	$( "#Project_container" ).animate({
+	    height: total_height + rows * 16 + "px"
+	  }, 500);
+
+}
